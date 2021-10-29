7f2b26741b02c0f89bb50c7a7e7fb95308ca6504
lieta96 <julieta.garcia96@gmai.com>
2021-10-29 10:22:52 -0300

    footer

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+
+import './footer.css'
+
+export default function Footer (){
+    return (
+        <footer class="position-absolute fixed-bottom w-100 py-4 flex-shrink-0">
+        <div class="container py-4">
+           <h5 className="text-light">Coded by <a className="text-light" href="https://github.com/lieta96" target="_blank">Lieta</a></h5>
+        </div>
+    </footer>
+    )
+}
